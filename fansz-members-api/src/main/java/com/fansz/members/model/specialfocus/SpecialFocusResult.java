8fe4994ffffa3b304e29b726bbfcb05e66aabe62
allan <ymyan@myfansz.com>
2015-12-10 17:48:19 +0800

    upgrade mybatis to latest stable version

--- fansz-members-api/src/main/java/com/fansz/members/model/specialfocus/SpecialFocusResult.java
+++ fansz-members-api/src/main/java/com/fansz/members/model/specialfocus/SpecialFocusResult.java
@@ -1,5 +1,7 @@
 package com.fansz.members.model.specialfocus;
 
+import com.fansz.members.model.fandom.FandomInfoResult;
+import com.fansz.members.model.profile.UserInfoResult;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import java.io.Serializable;
@@ -7,32 +9,51 @@ import java.io.Serializable;
 /**
  * Created by dell on 2015/12/9.
  */
-public class SpecialFocusResult implements Serializable{
+public class SpecialFocusResult implements Serializable {
 
     private static final long serialVersionUID = 5119301795049966905L;
 
     @JsonProperty("special_id")
     private Long id;
 
+    @JsonProperty("postion_tag")
+    private Long postionTag;
+
     @JsonProperty("special_fandom")
-    private SpecialFandomResult specialFandomResult;
+    private FandomInfoResult fandomInfoResult;
 
     @JsonProperty("special_member")
-    private SpecialMemberResult specialMemberResult;
+    private UserInfoResult userInfoResult;
 
-    public SpecialFandomResult getSpecialFandomResult() {
-        return specialFandomResult;
+    public Long getId() {
+        return id;
     }
 
-    public void setSpecialFandomResult(SpecialFandomResult specialFandomResult) {
-        this.specialFandomResult = specialFandomResult;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public SpecialMemberResult getSpecialMemberResult() {
-        return specialMemberResult;
+    public Long getPostionTag() {
+        return postionTag;
     }
 
-    public void setSpecialMemberResult(SpecialMemberResult specialMemberResult) {
-        this.specialMemberResult = specialMemberResult;
+    public void setPostionTag(Long postionTag) {
+        this.postionTag = postionTag;
+    }
+
+    public FandomInfoResult getFandomInfoResult() {
+        return fandomInfoResult;
+    }
+
+    public void setFandomInfoResult(FandomInfoResult fandomInfoResult) {
+        this.fandomInfoResult = fandomInfoResult;
+    }
+
+    public UserInfoResult getUserInfoResult() {
+        return userInfoResult;
+    }
+
+    public void setUserInfoResult(UserInfoResult userInfoResult) {
+        this.userInfoResult = userInfoResult;
     }
 }
