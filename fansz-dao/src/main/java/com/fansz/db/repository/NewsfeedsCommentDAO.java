d93947d89ad7ebda92ee62f299d59475a7fb3115
heli <lhe@myfansz.com>
2015-12-28 17:36:20 +0800

    添加朋友圈评论功能

---
+++ fansz-dao/src/main/java/com/fansz/db/repository/NewsfeedsCommentDAO.java
@@ -0,0 +1,17 @@
+package com.fansz.db.repository;
+
+import com.fansz.db.entity.NewsfeedsPostComment;
+import com.fansz.orm.dao.IBaseDAO;
+import com.fansz.orm.dao.annotation.DAO;
+import com.fansz.orm.dao.annotation.NamedQuery;
+
+/**
+ * Created by dell on 2015/12/28.
+ */
+@DAO("newsfeedsCommentDAO")
+public interface NewsfeedsCommentDAO  extends IBaseDAO<NewsfeedsPostComment> {
+
+    @NamedQuery(queryId = "newsfeeds.removeCommetByPostId", parameters = "postId")
+    int removeCommetByPostId(Long postId);
+
+}
