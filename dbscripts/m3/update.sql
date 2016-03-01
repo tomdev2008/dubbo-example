6a7dffbde584ae20cf5a940f8982be4cdf07c1a0
allan <ymyan@myfansz.com>
2016-03-01 11:33:44 +0800

    update table

--- dbscripts/m3/update.sql
+++ dbscripts/m3/update.sql
@@ -1,11 +1,13 @@
-ALTER TABLE feeds_member_post_fandom ADD post_level varchar(10);
-ALTER TABLE connects_member_profile ADD country varchar(10) comment '国别';
-ALTER TABLE feeds_fandom ADD level varchar(10) comment '级别';
+ALTER TABLE feeds_member_post_fandom ADD post_level VARCHAR(10);
+ALTER TABLE connects_member_profile ADD country VARCHAR(10) COMMENT '国别' DEFAULT 'CHN';
+ALTER TABLE connects_member_profile ADD level VARCHAR(10) COMMENT '级别';
+ALTER TABLE feeds_fandom ADD level VARCHAR(10) COMMENT '级别';
 CREATE TABLE t_pub_code (
-  id INTEGER AUTO_INCREMENT COMMENT '主键',
-  code_type VARCHAR(10) COMMENT '代码类别',
-  code_name VARCHAR(20) COMMENT '代码名称',
-  code_value VARCHAR(10) COMMENT '代码值',
+  id            INTEGER AUTO_INCREMENT
+  COMMENT '主键',
+  code_type     VARCHAR(10) COMMENT '代码类别',
+  code_name     VARCHAR(20) COMMENT '代码名称',
+  code_value    VARCHAR(10) COMMENT '代码值',
   display_order INTEGER DEFAULT 0,
   PRIMARY KEY (id)
 );
